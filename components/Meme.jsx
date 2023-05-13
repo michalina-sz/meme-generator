9f3ebc251cae1c6708993703caec7a04d4287a99
Michalina Szostak <michalinaszostakk@gmail.com>
2023-05-13 16:00:23 +0200

    Added Meme component

---
+++ components/Meme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Meme() {
+	return (
+		<form className='form'>
+			<div className='form--inputs'>
+				<input type='text'></input>
+				<input type='text'></input>
+			</div>
+			<button>Get a new meme image </button>
+		</form>
+	);
+}
