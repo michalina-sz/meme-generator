a25423dc0f4f1ef5a8a99cab8737b7b7e6bb0713
Michalina <michalina@Michalinas-MacBook-Air.local>
2023-05-15 10:26:20 +0200

    Added form styling and started working on getting images from memesData.js

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import memesData from '../memesData.js';
 
 export default function Meme() {
+	let url;
+
+	function GetMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		url = memesArray[randomNumber].url;
+		console.log(url);
+	}
+
 	return (
-		<form className='form'>
-			<div className='form--inputs'>
-				<input type='text'></input>
-				<input type='text'></input>
+		<main>
+			<p>{url}</p>
+			<div className='form'>
+				<input type='text' placeholder='Top text' className='form--input' />
+				<input type='text' placeholder='Bottom text' className='form--input' />
+				<button className='form--button' onClick={GetMemeImage}>
+					Get a new meme image 🖼
+				</button>
 			</div>
-			<button>Get a new meme image </button>
-		</form>
+		</main>
 	);
 }
